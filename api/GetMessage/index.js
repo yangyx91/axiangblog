c1da9ba4967a450ca922c8f0457ae5fa06d4e2d0
阿象 <yang.yx91@live.cn>
2020-07-20 18:24:02 +0800

    fetch api加入

--- api/GetMessage/index.js
+++ api/GetMessage/index.js
@@ -1,5 +1,6 @@
 module.exports = async function (context, req) {
     context.log('JavaScript HTTP trigger function processed a request.');
+   
 
     context.res = {
         body: {
@@ -18,4 +19,9 @@ module.exports = async function (context, req) {
     //         body: "Please pass a name on the query string or in the request body"
     //     };
     // }
+
+    //
+
+    
+
 };
